da1d53b32bc4e5422e8816a289c3cc408f2069e2
arieltraver <arieltraver@users.noreply.github.com>
2023-06-23 00:14:01 -0700

    port ping

--- portscan.py
+++ portscan.py
@@ -1,12 +1,16 @@
 from scapy.all import *
 
-def pingscan(first3bytes, start, end):
+def pingscan(first3bytes, start, end, wait_time=2):
+    responses = []
     for i in range(start, end):
         packet = IP(dst= first3bytes + str(i))/ICMP()
-        reply = sr1(packet)
+        reply = sr1(packet, timeout=wait_time)
         if reply is not None and first3bytes in reply.src:
             print(reply.src, "responded")
+            responses.append(reply.src)
+    for response in responses:
+        print(response)
 
-pingscan("192.168.1.", 0, 20)
+pingscan("192.168.1.", 0, 5)
 
 
