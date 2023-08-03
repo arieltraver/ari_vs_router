dc4eac007ee98f80e0811614cfbe566620fb044a
arieltraver <arieltraver@users.noreply.github.com>
2023-08-03 03:57:33 +0300

    half-open scan works and it detected that 22 was open on a nearby device, and the rest were closed

--- portscan.py
+++ portscan.py
@@ -27,9 +27,9 @@ def synscan(first3bytes, start, end, s_port=8085, d_port=80, wait_time=2):
         print(response)
 
 
-synscan("192.168.1.", 0, 5)
+#synscan("192.168.1.", 0, 5)
 
-def checkPortsTCP(ip, s_port, d_port_list, wait_time): #you can provide a list of commonly used TCP ports.
+def checkPortsTCP(ip, d_port_list, s_port=8085, wait_time=2): #you can provide a list of commonly used TCP ports.
     open_ports = []
     for d_port in d_port_list:
         ip_layer = IP(dst=ip)
@@ -37,8 +37,13 @@ def checkPortsTCP(ip, s_port, d_port_list, wait_time): #you can provide a list o
         packet = ip_layer / tcp_layer
         response = sr1(packet, timeout=wait_time)
         if response is not None and ip in response.src:
-            print(response.src, "is listening on port", d_port)
-            open_ports.append(d_port)
+            if response[TCP].flags == 0x12:
+                print(response.src, "is listening on port", d_port)
+                open_ports.append(d_port)
+            else:
+                print(response.src, "is awake but port", d_port, "is closed")
     for open_port in open_ports:
-        print(response)
-        
+        print(open_port)
+
+common_TCP_ports = [25, 80, 443, 20, 21, 23, 143, 3389, 22, 53, 110]
+checkPortsTCP("192.168.1.3", common_TCP_ports)
